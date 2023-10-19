8d93ceedf6bc1a6dda9065eb4317792f69ac6e8e
Joud Awad <joud.awad@metalabdesign.com>
2023-10-19 14:51:51 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const axios = require("axios");
+
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the Home page of package 2!");
+});
+
+app.get("/dummy", (req, res) => {
+  res.json({ message: "Dummy response from package 2" });
+});
+
+app.get("/call-endpoint", async (req, res) => {
+  try {
+    const response = await axios.get("http://app-1.retail-store");
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ message: "Failed to call endpoint" });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
